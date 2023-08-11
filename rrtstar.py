f4c20433b40ed313a9773cd3ecea4bfcacdbc5dd
Apoorva Khairnar <68499649+apoorvakhairnar@users.noreply.github.com>
2023-08-11 15:19:42 -0400

    Update rrtstar.py

--- rrtstar.py
+++ rrtstar.py
@@ -2,6 +2,10 @@
 Path planning Sample Code with RRT*
 author: Atsushi Sakai(@Atsushi_twi)
 """
+"""
+Additional changes have been made to the original code for the purpose of this project
+author: Apoorva Khairnar
+"""
 
 import math
 import sys
